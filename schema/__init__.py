a46103c2e830e15fd5330cd1c256feb19fbfe01c
ustimenko <ustimenko@nsgc.ru>
2026-04-28 16:50:34 +0300

    features: add yandex and google auth

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,5 +1,13 @@
 from schema.user import UserLoginSchema, UserCreateSchema
 from schema.task import TaskCreateSchema, TaskSchema
+from schema.auth import GoogleUserData, YandexUserData
 
 
-__all__ = ['UserLoginSchema', 'UserCreateSchema', 'TaskCreateSchema', 'TaskSchema']
+__all__ = [
+    'UserLoginSchema',
+    'UserCreateSchema',
+    'TaskCreateSchema',
+    'TaskSchema', 
+    'GoogleUserData', 
+    'YandexUserData'
+]
